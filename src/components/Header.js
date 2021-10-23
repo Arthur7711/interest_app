92245eb5f5b84fbf80ad550b76e05a36b199de35
Arthur Ghazaryan <newartroll11@gmail.com>
2021-10-24 01:17:17 +0400

    feat:(state value from mail)

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,9 @@ import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import FormDialog from './hiddenWindow'
 
 const styles = {
   root: {
@@ -34,7 +34,8 @@ function ButtonAppBar(props) {
           >
             <MenuIcon />
           </IconButton>
-          <Button color="inherit">Login</Button>
+          {/* <Button color="inherit">Login</Button> */}
+          <FormDialog />
         </Toolbar>
       </AppBar>
     </div>
